085fb39f48b4281469cd9045882a0c30693c9b56
Henrique Lucena Soares Silva <hlsilva@qualicorp.com.br>
2020-10-27 00:29:35 -0300

    add functions to execute get function and set result value in cache

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ class QRedisClient extends redis.RedisClient {
     constructor(client, options) {
         super();
         this._client = client;
-        this._options = options;
+        this._options = options || {};
+
+        if (this._options.enableCacheOnRunGet !== false) this._options.enableCacheOnRunGet = true;
     }
 
     getAsync(...args) {
@@ -24,6 +26,14 @@ class QRedisClient extends redis.RedisClient {
         return promisify(this.setex).bind(this)(...args);
     }
 
+    getsetAsync(...args) {
+        return promisify(this.getset).bind(this)(...args);
+    }
+
+    delAsync(...args) {
+        return promisify(this.del).bind(this)(...args);
+    }
+
     setAsync(key, value, timeout) {
         if (typeof value === 'object') value = JSON.stringify(value);
         if (value === null || value === undefined) value = '';
@@ -33,6 +43,60 @@ class QRedisClient extends redis.RedisClient {
         }
 
         return this.setexAsync(key, timeout, value);
+    }
+
+    async runGet(keyInfo, parameters, fnExecuteGet, options = {}) {
+        let value = null;
+
+        if (this._options.enableCacheOnRunGet) {
+            const key = `${keyInfo.namespace}#${parameters.join('_')}`;
+
+            if (options.asObject) {
+                value = await this.getAsObjAsync(key);
+            } else {
+                value = await this.getAsync(key);
+            }
+
+            if (value) {
+                // eslint-disable-next-line no-console
+                if (this._options.enableLog) console.info('[REDIS]', keyInfo.namespace, `exists in cache`);
+            } else {
+                // eslint-disable-next-line no-console
+                if (this._options.enableLog) console.info('[REDIS]', keyInfo.namespace, `not exists in cache`);
+
+                value = await fnExecuteGet();
+                await this.setAsync(key, value, keyInfo.timeout);
+            }
+        } else {
+            value = await fnExecuteGet();
+        }
+
+        if (options.parse && typeof options.parse === 'function') {
+            value = options.parse(value);
+        }
+
+        return value;
+    }
+
+    async runGetInt(keyInfo, parameters, fnExecuteGet, options = {}) {
+        return await this.runGet(keyInfo, parameters, fnExecuteGet, {
+            ...options,
+            parse: parseInt,
+        });
+    }
+
+    async runGetFloat(keyInfo, parameters, fnExecuteGet, options = {}) {
+        return await this.runGet(keyInfo, parameters, fnExecuteGet, {
+            ...options,
+            parse: parseFloat,
+        });
+    }
+
+    async runGetObj(keyInfo, parameters, fnExecuteGet, options = {}) {
+        return await this.runGet(keyInfo, parameters, fnExecuteGet, {
+            ...options,
+            asObject: true,
+        });
     }
 }
 
@@ -44,8 +108,9 @@ module.exports.createClient = function(options, ...args) {
 
     const date = new Date();
     const dateFormated = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+
     // eslint-disable-next-line no-console
-    client.on("ready", () => console.error(`[REDIS] conected on ${optionsAux.host}:${optionsAux.port} at ${dateFormated}`));
+    client.on("ready", () => console.info(`[REDIS] conected on ${optionsAux.host}:${optionsAux.port} at ${dateFormated}`));
 
     // eslint-disable-next-line no-console
     client.on("error", (error) => console.error('[REDIS]', error));
@@ -53,4 +118,15 @@ module.exports.createClient = function(options, ...args) {
     return new QRedisClient(client, {
         ...optionsAux,
     });
+};
+
+module.exports.TIMER = {
+    // seconds
+    s: (value) => value,
+
+    // minutes
+    m: (value) => 60 * value,
+
+    // hours
+    h: (value) => 60 * 60 * value,
 };
